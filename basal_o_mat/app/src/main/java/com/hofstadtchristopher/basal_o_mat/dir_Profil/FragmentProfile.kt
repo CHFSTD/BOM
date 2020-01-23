229e0307510d227801ea03f1497bc0a05d4c9a96
CHFSTD <chris.hofstadt@googlemail.com>
2020-01-23 12:51:04 +0100

    -- QUICKSAVE: -TODO modify new BasalProfil

--- basal_o_mat/app/src/main/java/com/hofstadtchristopher/basal_o_mat/dir_Profil/FragmentProfile.kt
+++ basal_o_mat/app/src/main/java/com/hofstadtchristopher/basal_o_mat/dir_Profil/FragmentProfile.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.hofstadtchristopher.basal_o_mat.R
 import com.hofstadtchristopher.basal_o_mat.viewModel.FProfileViewModel
+import kotlinx.android.synthetic.main.fragment_profile.*
 
 /**
  * A simple [Fragment] subclass.
@@ -27,5 +29,12 @@ class FragmentProfile : Fragment() {
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fProfil_fab.setOnClickListener {
+            Navigation.findNavController(it).navigate(FragmentProfileDirections.actionToFragmentAddBasalRate())
+        }
+    }
 
 }
